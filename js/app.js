bdb6ac03b195464f78538245935e485b24e6d1f9
Adnan Sawas <adnsawas@gmail.com>
2018-04-07 21:03:50 +0300

    Fixed issues and added the reset game functionality

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
  * Create a list that holds all of your cards
  */
 
+let openedCards = [];
+let matchesCount = 0;
+let attempts = 0;
+let unmatchedCardsTimerIsRunning = false;
+let shuffeledContent = [];
+
 const cardsContent =   ["fa-diamond",
 						"fa-paper-plane-o",
 						"fa-heart",
@@ -11,34 +17,129 @@ const cardsContent =   ["fa-diamond",
 						"fa-camera",
 						"fa-money"];
 
-let shuffeledContent = [];
-for (var i = 0; i < cardsContent.length; i++) {
-	shuffeledContent.push(cardsContent[i]);
-	shuffeledContent.push(cardsContent[i]);
+resetGame();
+
+// for (var i = 0; i < cardsContent.length; i++) {
+// 	shuffeledContent.push(cardsContent[i]);
+// 	shuffeledContent.push(cardsContent[i]);
+// }
+
+// shuffeledContent = shuffle(shuffeledContent);
+
+// let virtualDeck = document.createDocumentFragment();
+
+// for (let i = 0; i < 16; i++) {
+// 	let card1 = document.createElement("li");
+// 	let card1Content = document.createElement("i");
+
+// 	card1Content.classList.add("fa");
+// 	card1Content.classList.add(shuffeledContent[i]);
+
+// 	card1.classList.add("card");
+
+// 	// This is for testing only, remove when game is complete
+// 	//card1.classList.add("show");
+// 	card1.appendChild(card1Content);
+// 	virtualDeck.appendChild(card1);
+// }
+
+//  // Add the shuffeled cards into the deck
+//  document.getElementsByClassName("deck")[0].appendChild(virtualDeck);
+
+ document.getElementsByClassName("deck")[0].addEventListener("click", function(event) {
+ 	if (unmatchedCardsTimerIsRunning) {return;}
+ 	let clickedTarget;
+ 	if (event.target.tagName == "UL") // User clicked on the canvas but not on a card
+ 	{
+ 		return;
+ 	}
+ 	else if (event.target.tagName == "I") // User clicked exactly on the card symbol
+ 	{
+ 		clickedTarget = event.target.parentElement;
+ 	}
+ 	else // User for sure clicked on the card
+ 	{
+ 		clickedTarget = event.target;
+ 	}
+
+ 	// clickedTarget is now pointing to the right clicked card
+
+ 	// First open the card by adding the show and open classes
+ 	clickedTarget.classList.add("show");
+ 	clickedTarget.classList.add("open");
+
+ 	// Add the opened card to the openedCards array
+ 	openedCards.push(clickedTarget);
+
+ 	// Check if the clicked card is the first or second card
+ 	if (openedCards.length == 1) {return;}
+ 	if (openedCards.length == 2) {
+
+ 		// Check that the user did not click the same card twice
+ 		if (openedCards[0] == openedCards[1])
+ 		{
+ 			openedCards.pop();
+ 			return;
+ 		}
+
+ 		document.getElementsByClassName("moves")[0].innerText = ++attempts;
+ 		// The user selected the second card and it is time to evaluate
+ 		if (isCardsMatched()) {
+
+ 			
+ 			console.log("Match");
+
+ 			// Keep the card open and mark it as match
+ 			openedCards[0].classList.remove("open");
+ 			openedCards[0].classList.add("match");
+
+ 			openedCards[1].classList.remove("open");
+ 			openedCards[1].classList.add("match");
+
+ 			// Increase the match counter
+ 			matchesCount++;
+
+ 			openedCards.pop();
+ 			openedCards.pop();
+
+ 			if (matchesCount >= 8) { // The winning condition
+ 				console.log("You Won !");
+ 				document.getElementById("winning").showModal();
+ 			}
+ 			
+ 		}
+ 		else {
+ 			console.log("Not Match");
+
+ 			// Animate here if possible
+
+ 			unmatchedCardsTimerIsRunning = true;
+ 			// Close the two cards after 0.5 second
+ 			setTimeout(function(){ 
+	 			openedCards[0].classList.remove("open");
+	 			openedCards[0].classList.remove("show");
+
+	 			openedCards[1].classList.remove("open");
+	 			openedCards[1].classList.remove("show"); 
+
+				// Remove the open cards from the openedCards array to prepare for the next round
+			 	openedCards.pop();
+			 	openedCards.pop();
+			 	unmatchedCardsTimerIsRunning = false;
+			 }, 750);
+
+ 				
+ 		}
+
+ 		
+ 	}
+ });
+
+function isCardsMatched() {
+	let isMatch = false;
+	return (openedCards[0].children[0].className == openedCards[1].children[0].className);
+
 }
-
-shuffeledContent = shuffle(shuffeledContent);
-
-let virtualDeck = document.createDocumentFragment();
-
-for (let i = 0; i < 16; i++) {
-	let card1 = document.createElement("li");
-	let card1Content = document.createElement("i");
-
-	card1Content.classList.add("fa");
-	card1Content.classList.add(shuffeledContent[i]);
-
-	card1.classList.add("card");
-
-	// This is for testing only, remove when game is complete
-	card1.classList.add("show");
-	card1.appendChild(card1Content);
-	virtualDeck.appendChild(card1);
-}
-
- // Add the shuffeled cards into the deck
- document.getElementsByClassName("deck")[0].appendChild(virtualDeck);
-
 
 
 /*
@@ -61,6 +162,53 @@ function shuffle(array) {
     }
 
     return array;
+}
+
+function resetGame() {
+	if (document.getElementById("winning").open) {
+		document.getElementById("winning").close();
+	}
+
+	// Reset all deck cards
+	if (document.getElementsByClassName("deck")[0].children.length > 0) {
+		let currentCards = document.getElementsByClassName("card");
+		for (let i = 0; i < 16; i++) {
+			currentCards[0].remove();
+		}
+	}
+
+	openedCards = [];
+	matchesCount = 0;
+	attempts = 0;
+	unmatchedCardsTimerIsRunning = false;
+	shuffeledContent = [];
+
+	for (var i = 0; i < cardsContent.length; i++) {
+		shuffeledContent.push(cardsContent[i]);
+		shuffeledContent.push(cardsContent[i]);
+	}
+
+	shuffeledContent = shuffle(shuffeledContent);
+
+	let virtualDeck = document.createDocumentFragment();
+
+	for (let i = 0; i < 16; i++) {
+		let card1 = document.createElement("li");
+		let card1Content = document.createElement("i");
+
+		card1Content.classList.add("fa");
+		card1Content.classList.add(shuffeledContent[i]);
+
+		card1.classList.add("card");
+
+		// This is for testing only, remove when game is complete
+		//card1.classList.add("show");
+		card1.appendChild(card1Content);
+		virtualDeck.appendChild(card1);
+	}
+
+	 // Add the shuffeled cards into the deck
+	 document.getElementsByClassName("deck")[0].appendChild(virtualDeck);
 }
 
 
