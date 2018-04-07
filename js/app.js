b646113c2f82a90cbe1abb045510d789379de839
Adnan Sawas <adnsawas@gmail.com>
2018-04-07 14:32:08 +0300

    Added the cards into the deck. Shuffling is not done yet.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,51 @@
  * Create a list that holds all of your cards
  */
 
+const cardsContent = ["fa-diamond",
+						"fa-paper-plane-o",
+						"fa-heart",
+						"fa-home",
+						"fa-bicycle",
+						"fa-car",
+						"fa-camera",
+						"fa-money"];
+
+let virtualDeck = document.createDocumentFragment();
+
+for (let i = 0; i < 16; i = i + 2) {
+	let card1 = document.createElement("li");
+	let card2 = document.createElement("li");
+	let card1Content = document.createElement("i");
+	let card2Content = document.createElement("i");
+
+	card1Content.classList.add("fa");
+	card1Content.classList.add(cardsContent[i/2]);
+	
+	card2Content.classList.add("fa");
+	card2Content.classList.add(cardsContent[i/2]);
+
+	card1.classList.add("card");
+	card2.classList.add("card");
+
+	// This is for testing only, remove when game is complete
+	card1.classList.add("show");
+	card2.classList.add("show");
+
+	card1.appendChild(card1Content);
+	card2.appendChild(card2Content);
+
+	virtualDeck.appendChild(card1);
+	virtualDeck.appendChild(card2);
+}
+
+
+ //let shuffeledDeck = document.createDocumentFragment();
+ //shuffeledDeck.children =  shuffle(virtualDeck.children);
+
+ // Add the shuffeled cards into the deck
+ document.getElementsByClassName("deck")[0].appendChild(virtualDeck);
+
+
 
 /*
  * Display the cards on the page
